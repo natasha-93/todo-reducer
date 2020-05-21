975e172235fd987e687e8a472a3650f2d88577a0
Natasha Lawson <natasha.lawson93@gmail.com>
2020-05-21 17:06:57 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import { v4 as uuid } from "uuid";
+
+type Todo = {
+  id: string;
+  text: string;
+  isCompleted: boolean;
+};
+
+type TodosState = {
+  todos: Todo[];
+  newTodoText: string;
+};
+
+type TodosAction =
+  | { type: "ADD_TODO" }
+  | { type: "TOGGLE_TODO"; payload: string }
+  | { type: "EDIT_TODO"; payload: { id: string; text: string } }
+  | { type: "DELETE_TODO"; payload: string }
+  | { type: "EDIT_NEW_TODO"; payload: string };
+
+function reducer(state: TodosState, action: TodosAction): TodosState {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        newTodoText: "",
+        todos: [
+          ...state.todos,
+          {
+            id: uuid(),
+            text: state.newTodoText,
+            isCompleted: false,
+          },
+        ],
+      };
+    default:
+      return state;
+    case "EDIT_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, text: action.payload.text }
+            : todo
+        ),
+      };
+    case "EDIT_NEW_TODO": {
+      return {
+        ...state,
+        newTodoText: action.payload,
+      };
+    }
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, isCompleted: !todo.isCompleted }
+            : todo
+        ),
+      };
+  }
+}
 
 function App() {
+  const [{ todos, newTodoText }, dispatch] = useReducer(reducer, {
+    newTodoText: "",
+    todos: [],
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Todo List</p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "ADD_TODO" });
+        }}
+      >
+        <input
+          placeholder="Enter todo..."
+          value={newTodoText}
+          onChange={(e) =>
+            dispatch({ type: "EDIT_NEW_TODO", payload: e.target.value })
+          }
+        />
+        <button>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              onClick={() => {
+                dispatch({ type: "TOGGLE_TODO", payload: todo.id });
+              }}
+            />
+            <input
+              value={todo.text}
+              onChange={(e) => {
+                dispatch({
+                  type: "EDIT_TODO",
+                  payload: { id: todo.id, text: e.target.value },
+                });
+              }}
+              style={{
+                border: "none",
+                textDecoration: todo.isCompleted ? "line-through" : "none",
+              }}
+            />
+            <button
+              onClick={(e) =>
+                dispatch({ type: "DELETE_TODO", payload: todo.id })
+              }
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
